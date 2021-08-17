c5b139fdf7a7b0bb411e485fec92a6c4bc092989
Hamzah Fayad <hamzahfayad99@gmail.com>
2021-08-17 13:46:19 +0200

    initial

---
+++ index.js
@@ -0,0 +1,21 @@
+{
+    "use strict";
+
+    console.log("...");
+
+    var html = document.querySelector("html");
+
+    function check() {
+        var switcher = document.querySelector('input[type=checkbox]');
+
+        if (switcher.checked) {
+            html.classList.remove("theme-light");
+            html.classList.add("theme-dark");
+        }
+        else {
+            html.classList.add("theme-light");
+            html.classList.remove("theme-dark");
+        }
+    }
+
+}
