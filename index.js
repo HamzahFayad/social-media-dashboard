56abb0d6b4103bf0221d52093c8121a04be2e420
Hamzah Fayad <hamzahfayad99@gmail.com>
2021-08-18 16:34:38 +0200

    animation + color theme

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 {
-    "use strict";
+  ("use strict");
 
-    console.log("...");
+  console.log("...");
 
-    var html = document.querySelector("html");
+  var html = document.querySelector("html");
 
-    function check() {
-        var switcher = document.querySelector('input[type=checkbox]');
+  function check() {
+    let switcher = document.querySelector("input[type=checkbox]");
+    let card1 = document.querySelector(".card1");
 
-        if (switcher.checked) {
-            html.classList.add("theme-light");
-            html.classList.remove("theme-dark");
-        }
-        else {
-            html.classList.remove("theme-light");
-            html.classList.add("theme-dark");
-
-        }
+    if (switcher.checked) {
+      html.classList.add("theme-light");
+      html.classList.remove("theme-dark");
+    } else {
+      html.classList.remove("theme-light");
+      html.classList.add("theme-dark");
     }
-
+  }
 }
